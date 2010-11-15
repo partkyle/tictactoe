900d48e0d8cadd784f7992f9130393865fb7e023
partkyle <partkyle@gmail.com>
2010-11-14 16:44:35 -0800

    moved AI to util package

---
+++ app/util/TicTacToeAI.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import models.Move;
+import models.Player;
+
+public class TicTacToeAI {
+	public static Move findNextMove(Player[][] state){
+		Move m = new Move();
+		m.x = 0;
+		m.y = 0;
+		m.player = Player.Computer;
+		return m;
+	}
+}
