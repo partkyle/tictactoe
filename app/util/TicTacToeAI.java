403d6cfae08e623b5c1faf8d1f84a394e0bb6025
partkyle <partkyle@gmail.com>
2010-11-14 16:45:01 -0800

    format only

--- app/util/TicTacToeAI.java
+++ app/util/TicTacToeAI.java
@@ -4,7 +4,7 @@ import models.Move;
 import models.Player;
 
 public class TicTacToeAI {
-	public static Move findNextMove(Player[][] state){
+	public static Move findNextMove(Player[][] state) {
 		Move m = new Move();
 		m.x = 0;
 		m.y = 0;
