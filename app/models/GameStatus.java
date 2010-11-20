d978f2f6925ac28af08c8a467ff8aad7823ca03f
partkyle <partkyle@gmail.com>
2010-11-20 01:24:08 -0800

    added colors to the chart URL

--- app/models/GameStatus.java
+++ app/models/GameStatus.java
@@ -1,5 +1,23 @@
 package models;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 public enum GameStatus {
-	Incomplete, Draw, Win, Loss;
+	Incomplete("000000"), Win("00CC00"), Loss("FF0000"), Draw("DEDEDE");
+
+	public String color;
+
+	private GameStatus(String color) {
+		this.color = color;
+	}
+
+	public static String getColorURL(Collection<GameStatus> statuses) {
+		Iterator<GameStatus> iter = statuses.iterator();
+		String result = iter.next().color;
+		while (iter.hasNext()) {
+			result += "|" + iter.next().color;
+		}
+		return result;
+	}
 }
