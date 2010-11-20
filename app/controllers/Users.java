2ac5d86ad86ee33a5ae8af24561459e087273021
partkyle <partkyle@gmail.com>
2010-11-20 03:12:45 -0800

    added user edit support

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import models.User;
-import play.mvc.*;
+import play.data.validation.Email;
+import play.data.validation.Equals;
+import play.i18n.Messages;
+import play.libs.Codec;
 
 public class Users extends Application {
 
@@ -12,6 +15,30 @@ public class Users extends Application {
 	public static void show(String username) {
 		User user = User.findByUsername(username);
 		render(user);
+	}
+
+	public static void edit(String username) {
+		if (!username.equals(session.get("username")))
+			forbidden("You don't have access to that user.");
+		User user = User.findByUsername(username);
+		render(user);
+	}
+
+	public static void update(String username, @Email String email, String password, @Equals("password") String password2) {
+		if (!username.equals(session.get("username")))
+			forbidden("You don't have access to that user.");
+		if (validation.hasErrors()) {
+			validation.keep();
+			params.flash();
+			flash.error("Please correct these errors !");
+			edit(username);
+		}
+		User user = User.findByUsername(username);
+		user.email = email;
+		if (password != null)
+			user.password = Codec.hexSHA1(password);
+		user.save();
+		show(user.username);
 	}
 
 }
