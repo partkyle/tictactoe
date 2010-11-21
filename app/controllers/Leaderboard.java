96f37d5037e6cb523cbcd25c029dc24212791626
partkyle <partkyle@gmail.com>
2010-11-20 23:39:18 -0800

    paging for ranks

--- app/controllers/Leaderboard.java
+++ app/controllers/Leaderboard.java
@@ -6,9 +6,15 @@ import models.Ranking;
 
 public class Leaderboard extends Application {
 
-	public static void show() {
-		List<Ranking> rankings = Ranking.find("order by rank").fetch();
-		render(rankings);
+	public static void index() {
+		show(1);
+	}
+
+	public static void show(Integer page) {
+		List<Ranking> rankings;
+		Long count = Ranking.count();
+		rankings = Ranking.find("order by rank").from((page - 1) * pageSize).fetch(pageSize);
+		render(rankings, count, page);
 	}
 
 }
